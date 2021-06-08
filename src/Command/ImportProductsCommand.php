a431b23f4682e02b4bc844efaab71bb5da5cf0b8
Guillermo <guillermo@maspocovendo.com>
2021-06-07 21:46:30 -0300

    sync script finished

--- src/Command/ImportProductsCommand.php
+++ src/Command/ImportProductsCommand.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Serializer\Encoder\CsvEncoder;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -19,11 +20,13 @@ use Symfony\Component\PropertyInfo\Extractor\ReflectionExtractor;
 class ImportProductsCommand extends Command
 {
     protected static $defaultName = 'import-products';
+    protected $progressBar;
 
     public function __construct(EntityManagerInterface $em)
     {
         parent::__construct();
         $this->em = $em;
+        $this->productRepository = $this->em->getRepository(Product::class);
     }
 
     protected function configure()
@@ -33,54 +36,26 @@ class ImportProductsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // info about normalizers and encoders: https://symfony.com/doc/current/components/serializer.html
-
         $filename = '/home/usuario/proyectos/challenge/products.json';
-
-        /* ****************** */
-        /* streamed solution: */
-        /* ****************** */
-
-        // setting up serializer
-        $normalizers = array(
-            new ObjectNormalizer(null,null,null, new ReflectionExtractor()),
-            new ArrayDenormalizer(),
-        );
-        $encoders = [new JsonEncoder()];
-        $serializer = new Serializer($normalizers, $encoders);
 
         // using https://github.com/halaxa/json-machine to proccess objects one by one. Products are returned as arrays
         $products = JsonMachine::fromFile($filename);
 
-
         // update/create DB proucts
+        $foundProducts = 0;
         foreach ($products as $id => $product) {
-
-            // each product is an array so I have to use denormalize insted of deserialize
-            $productFromJson = $serializer->denormalize($product, Product::class, 'json');
-            $productFromDb = $this->em->getRepository(Product::class)->findOneByStyleNumber($productFromJson->getStyleNumber());
-
-            // if the product doesn's exist create it, otherwise update it
-            if (!$productFromDb){
-                $productFromJson->setSynchronized(false);
-                $this->em->persist($productFromJson);
-            }
-            else {
-                $dirty = $productFromDb->updateWith($productFromJson);
-                // if the product changed then save the changes, otherwise we don't do anything
-                if($dirty){
-                    $productFromDb->setSynchronized(false);
-                    $this->em->persist($productFromDb);
-                }
-            }
-
-            $this->em->flush();
+            $this->createOrUpdate($product);
+            $foundProducts++;
         }
+        $output->writeln("origin products found: $foundProducts");
 
-        // export to CSV
-        $productsFromDb = $this->em->getRepository(Product::class)->findAll();
+        // show how many unsync products were found
+        $unsyncProducts = $this->productRepository->findBySynchronized(false);
+        $output->writeln("unsynced products found: " . sizeof($unsyncProducts));
 
-        $this->toCSV($productsFromDb);
+        // export all products to CSV
+        $this->toCSV($this->productRepository->findAll());
+        $output->writeln("export to csv finished");
 
         return 1;
     }
@@ -95,10 +70,48 @@ class ImportProductsCommand extends Command
                 [$product], // using toString
                 ';' // The delimiter
             );
+            $product->setSynchronized(true);
+            $this->em->persist($product);
+            $this->em->flush();
+        }
+        fclose($fp);
+    }
+
+    private function createOrUpdate ($product){
+
+        $serializer = $this->setUpSerializer();
+
+        // each product is an array so I have to use denormalize insted of deserialize
+        $productFromJson = $serializer->denormalize($product, Product::class, 'json');
+        $productFromDb = $this->em->getRepository(Product::class)->findOneByStyleNumber($productFromJson->getStyleNumber());
+
+        // if the product doesn's exist create it, otherwise update it
+        if (!$productFromDb){
+            $productFromJson->setSynchronized(false);
+            $this->em->persist($productFromJson);
+        }
+        else {
+            $dirty = $productFromDb->updateWith($productFromJson);
+            // if the product changed then save the changes, otherwise we don't do anything
+            if($dirty){
+                $productFromDb->setSynchronized(false);
+                $this->em->persist($productFromDb);
+            }
         }
 
-        fclose($fp);
+        $this->em->flush();
+    }
 
+    private function setUpSerializer() {
+
+        // info about normalizers and encoders: https://symfony.com/doc/current/components/serializer.html
+        $normalizers = array(
+            new ObjectNormalizer(null,null,null, new ReflectionExtractor()),
+            new ArrayDenormalizer(),
+        );
+        $encoders = [new JsonEncoder()];
+
+        return new Serializer($normalizers, $encoders);
     }
 
 }
